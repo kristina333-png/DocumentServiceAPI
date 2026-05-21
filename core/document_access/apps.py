1879d0c5e5ae9034644fcc10e0260f0a367c6550
kris <kristinaastapova333@gmail.com>
2026-05-21 10:58:49 +0300

    Initial empty main branch

--- core/document_access/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentAccessConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core.document_access"
--- core/external_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentVersionConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core.external_system"
